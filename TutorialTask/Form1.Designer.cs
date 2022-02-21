b967b3a5efeb8aca8a1d2d12dba153c5fc1711b6
JESUS ANGEL CEPEDA RODRIGUEZ <jesusangelcepedarodriguez60@gmail.com>
2022-02-21 09:58:36 -0600

    Agregar archivos de proyecto.

---
+++ TutorialTask/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace TutorialTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.btnMax = new System.Windows.Forms.Button();
+            this.btnEsPrimo = new System.Windows.Forms.Button();
+            this.lstResultado = new System.Windows.Forms.ListBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(84, 53);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(71, 18);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Numero";
+            // 
+            // btnMax
+            // 
+            this.btnMax.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMax.Location = new System.Drawing.Point(174, 72);
+            this.btnMax.Name = "btnMax";
+            this.btnMax.Size = new System.Drawing.Size(85, 29);
+            this.btnMax.TabIndex = 1;
+            this.btnMax.Text = "Max";
+            this.btnMax.UseVisualStyleBackColor = true;
+            this.btnMax.Click += new System.EventHandler(this.btnMax_Click);
+            // 
+            // btnEsPrimo
+            // 
+            this.btnEsPrimo.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEsPrimo.Location = new System.Drawing.Point(265, 71);
+            this.btnEsPrimo.Name = "btnEsPrimo";
+            this.btnEsPrimo.Size = new System.Drawing.Size(108, 29);
+            this.btnEsPrimo.TabIndex = 2;
+            this.btnEsPrimo.Text = "¿Es primo?";
+            this.btnEsPrimo.UseVisualStyleBackColor = true;
+            this.btnEsPrimo.Click += new System.EventHandler(this.btnEsPrimo_Click);
+            // 
+            // lstResultado
+            // 
+            this.lstResultado.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstResultado.FormattingEnabled = true;
+            this.lstResultado.ItemHeight = 18;
+            this.lstResultado.Location = new System.Drawing.Point(36, 159);
+            this.lstResultado.Name = "lstResultado";
+            this.lstResultado.Size = new System.Drawing.Size(337, 130);
+            this.lstResultado.TabIndex = 3;
+            this.lstResultado.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(36, 74);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(132, 26);
+            this.txtNumero.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TutorialTask.Properties.Resources.numeros_primos;
+            this.ClientSize = new System.Drawing.Size(399, 303);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lstResultado);
+            this.Controls.Add(this.btnEsPrimo);
+            this.Controls.Add(this.btnMax);
+            this.Controls.Add(this.lblNumero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Button btnMax;
+        private System.Windows.Forms.Button btnEsPrimo;
+        private System.Windows.Forms.ListBox lstResultado;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
+
---
+++ TutorialTask/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TutorialTask
+{
+    public partial class Form1 : Form
+    {
+        private string EsPrimo(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if ((num % i) == 0)
+                {
+                    return string.Format("{0} no es primo", num);
+                }
+            }
+            return string.Format("{0} es primo", num);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnEsPrimo_Click(object sender, EventArgs e)
+        {
+            var resultado =await Task.Run( () => EsPrimo(int.Parse(txtNumero.Text)));
+            lstResultado.Items.Add(resultado);
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            txtNumero.Text = int.MaxValue.ToString();
+        }
+    }
+}
